1dd5f73b4edd35485eb3f8aa49bd7dd98a65c562
Yakhyokhuja Valikhujaev <yakhyo9696@gmail.com>
2024-08-05 11:06:24 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -9,8 +9,8 @@ fi
 rm -rf weights/*.onnx
 
 # Download the files and save them to the weights directory
-wget -O weights/w600k_r50.onnx https://github.com/yakhyo/face-analysis/releases/download/v0.0.1/w600k_r50.onnx
-wget -O weights/det_2.5g.onnx https://github.com/yakhyo/face-analysis/releases/download/v0.0.1/det_2.5g.onnx
-wget -O weights/det_500m.onnx https://github.com/yakhyo/face-analysis/releases/download/v0.0.1/det_500m.onnx
-wget -O weights/det_10g.onnx https://github.com/yakhyo/face-analysis/releases/download/v0.0.1/det_10g.onnx
-wget -O weights/genderage.onnx https://github.com/yakhyo/face-analysis/releases/download/v0.0.1/genderage.onnx
+wget -O weights/w600k_r50.onnx https://github.com/yakhyo/facial-analysis/releases/download/v0.0.1/w600k_r50.onnx
+wget -O weights/det_2.5g.onnx https://github.com/yakhyo/facial-analysis/releases/download/v0.0.1/det_2.5g.onnx
+wget -O weights/det_500m.onnx https://github.com/yakhyo/facial-analysis/releases/download/v0.0.1/det_500m.onnx
+wget -O weights/det_10g.onnx https://github.com/yakhyo/facial-analysis/releases/download/v0.0.1/det_10g.onnx
+wget -O weights/genderage.onnx https://github.com/yakhyo/facial-analysis/releases/download/v0.0.1/genderage.onnx
